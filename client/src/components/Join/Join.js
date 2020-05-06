a349e8872ea91259cbb222f5a6f34755a538ba4b
Bohan <chenbohan81@gmail.com>
2020-05-06 11:47:24 -0400

    style change for join page

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,25 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Button } from "@material-ui/core";
 
-import './Join.css';
+import "./Join.css";
 
 export default function SignIn() {
-  const [name, setName] = useState('');
-  const [room, setRoom] = useState('');
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
 
   return (
     <div className="joinOuterContainer">
       <div className="joinInnerContainer">
-        <h1 className="heading">Realtime Chat App</h1>
-        <h2 className="description">React, Express, Node and Socket.IO</h2>
+        <h1 className="heading">4 in a Row</h1>
         <div>
-          <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
+          <input
+            placeholder="Name"
+            className="joinInput"
+            type="text"
+            onChange={(event) => setName(event.target.value)}
+          />
         </div>
         <div>
-          <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
+          <input
+            placeholder="Room"
+            className="joinInput mt-20"
+            type="text"
+            onChange={(event) => setRoom(event.target.value)}
+          />
         </div>
-        <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
-          <button className={'button mt-20'} type="submit">Join</button>
+        <Link
+          onClick={(e) => (!name || !room ? e.preventDefault() : null)}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <Button
+            variant="contained"
+            color="primary"
+            className="button mt-20 removeUnderline"
+            type="submit"
+          >
+            play
+          </Button>
         </Link>
       </div>
     </div>
