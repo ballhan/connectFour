709c9a3a1651c120ad6100d0addc13abfa215a0b
Bohan <chenbohan81@gmail.com>
2020-05-10 16:51:06 -0400

    more game.js

--- client/src/components/Cell/Cell.js
+++ client/src/components/Cell/Cell.js
@@ -1,14 +1,12 @@
 import React from "react";
 import styles from "./Cell.module.css";
 
-const Cell = ({ value, columnIndex, play }) => {
+const Cell = ({ value, columnIndex, play, move }) => {
   return (
     <td>
       <div
         className={styles.cell}
-        onClick={() => {
-          play(columnIndex);
-        }}
+        onClick={(e) => (!move ? e.preventDefault() : play(columnIndex))}
       >
         <div className={value === 1 ? styles.red : styles.white}></div>
       </div>
--- client/src/components/Game/Game.js
+++ client/src/components/Game/Game.js
@@ -9,58 +9,61 @@ import io from "socket.io-client";
 let socket;
 
 const Game = ({ name, roomFull, users }) => {
-  const [player, setPlayer] = useState("");
-  const [opponent, setOpponent] = useState("");
-  const [firstPlay, setFirstPlay] = useState("");
-  const [currentPlay, setCurrentPlay] = useState("");
-  const [message, setMessage] = useState("");
-  const [board, setBoard] = useState([]);
-  const [gameOver, setGameOver] = useState("");
-
-  //set your name to player
-  useEffect(() => {
-    setPlayer(name);
-  }, [name]);
-
-  useEffect(() => {
-    if (!roomFull) {
-      setFirstPlay(player);
+  //init board
+  var boardArray = [];
+  for (let r = 0; r < 6; r++) {
+    let row = [];
+    for (let c = 0; c < 7; c++) {
+      row.push(null);
     }
-  }, [roomFull]);
+    boardArray.push(row);
+  }
 
+  const [board, setBoard] = useState(boardArray);
+
+  //set player to yourself
+  const [player, setPlayer] = useState(name);
+  const [opponent, setOpponent] = useState("");
+  const [currentPlayer, setCurrentPlayer] = useState("");
+  const [move, setMove] = useState("");
+  const [message, setMessage] = useState("");
+  const [gameOver, setGameOver] = useState(false);
+
+  //if roomfull, setup opponent name, status change if roomFull change
   useEffect(() => {
     if (roomFull) {
       setOpponent(users.filter((user) => user.name !== name)[0].name);
     }
   }, [roomFull]);
 
-  useEffect(() => {
-    if (player === firstPlay) {
-      setCurrentPlay(firstPlay);
-    }
-  }, [firstPlay]);
-
+  //if room is not full, set currentplay to yourself, you are the first to join
   useEffect(() => {
     if (!roomFull) {
-      setMessage(`Waiting for opponent to join`);
-    } else if (currentPlay === player) {
-      setMessage(`My turn`);
+      setCurrentPlayer(player);
     } else {
-      setMessage(`Waiting for ${opponent} to move`);
+      setCurrentPlayer(opponent);
     }
   }, [roomFull]);
 
-  // useEffect(() => {
-  //   let board = [];
-  //   for (let r = 0; r < 6; r++) {
-  //     let row = [];
-  //     for (let c = 0; c < 7; c++) {
-  //       row.push(null);
-  //     }
-  //     board.push(row);
-  //   }
-  //   setBoard(board);
-  // }, [board]);
+  //if you are current play, you are able to make move
+  useEffect(() => {
+    if (player === currentPlayer) {
+      setMove(true);
+    } else {
+      setMove(false);
+    }
+  }, [currentPlayer]);
+
+  //message
+  useEffect(() => {
+    if (!roomFull) {
+      setMessage(`Waiting for opponent to join`);
+    } else if (currentPlayer === player) {
+      setMessage(`My turn`);
+    } else {
+      setMessage(`Waiting for opponent to move`);
+    }
+  }, [roomFull]);
 
   console.log(
     "me:",
@@ -69,206 +72,166 @@ const Game = ({ name, roomFull, users }) => {
     roomFull,
     "opponent:",
     opponent,
-    "firstPlay:",
-    firstPlay,
     "currentplay:",
-    currentPlay
+    currentPlayer,
+    "move",
+    move
   );
 
-  return <h1>{message}</h1>;
+  const play = (c) => {
+    if (gameOver) {
+      // Place piece on board
+      var board = boardArray;
+      for (let r = 5; r >= 0; r--) {
+        if (!board[r][c]) {
+          board[r][c] = currentPlayer;
+          break;
+        }
+      }
+
+      // Check status of board
+      let result = checkAll(board);
+      if (result === player) {
+        setBoard(board);
+        setGameOver(true);
+        setMessage(`${player} WINS`);
+      } else if (result === opponent) {
+        setBoard(board);
+        setGameOver(true);
+        setMessage(`${opponent} WINS`);
+      } else if (result === "draw") {
+        setBoard(board);
+        setGameOver(true);
+        setMessage("Draw");
+      } else {
+        setBoard(board);
+        setCurrentPlayer(togglePlayer());
+      }
+    } else {
+      setMessage("Game over. Please start a new game.");
+    }
+  };
+
+  const togglePlayer = () => {
+    return currentPlayer === player ? opponent : player;
+  };
+
+  const checkVertical = (board) => {
+    // Check only if row is 3 or greater
+    for (let r = 3; r < 6; r++) {
+      for (let c = 0; c < 7; c++) {
+        if (board[r][c]) {
+          if (
+            board[r][c] === board[r - 1][c] &&
+            board[r][c] === board[r - 2][c] &&
+            board[r][c] === board[r - 3][c]
+          ) {
+            return board[r][c];
+          }
+        }
+      }
+    }
+  };
+
+  const checkHorizontal = (board) => {
+    // Check only if column is 3 or less
+    for (let r = 0; r < 6; r++) {
+      for (let c = 0; c < 4; c++) {
+        if (board[r][c]) {
+          if (
+            board[r][c] === board[r][c + 1] &&
+            board[r][c] === board[r][c + 2] &&
+            board[r][c] === board[r][c + 3]
+          ) {
+            return board[r][c];
+          }
+        }
+      }
+    }
+  };
+
+  const checkDiagonalRight = (board) => {
+    // Check only if row is 3 or greater AND column is 3 or less
+    for (let r = 3; r < 6; r++) {
+      for (let c = 0; c < 4; c++) {
+        if (board[r][c]) {
+          if (
+            board[r][c] === board[r - 1][c + 1] &&
+            board[r][c] === board[r - 2][c + 2] &&
+            board[r][c] === board[r - 3][c + 3]
+          ) {
+            return board[r][c];
+          }
+        }
+      }
+    }
+  };
+
+  const checkDiagonalLeft = (board) => {
+    // Check only if row is 3 or greater AND column is 3 or greater
+    for (let r = 3; r < 6; r++) {
+      for (let c = 3; c < 7; c++) {
+        if (board[r][c]) {
+          if (
+            board[r][c] === board[r - 1][c - 1] &&
+            board[r][c] === board[r - 2][c - 2] &&
+            board[r][c] === board[r - 3][c - 3]
+          ) {
+            return board[r][c];
+          }
+        }
+      }
+    }
+  };
+
+  const checkDraw = (board) => {
+    for (let r = 0; r < 6; r++) {
+      for (let c = 0; c < 7; c++) {
+        if (board[r][c] === null) {
+          return null;
+        }
+      }
+    }
+    return "draw";
+  };
+
+  const checkAll = (board) => {
+    return (
+      checkVertical(board) ||
+      checkDiagonalRight(board) ||
+      checkDiagonalLeft(board) ||
+      checkHorizontal(board) ||
+      checkDraw(board)
+    );
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.message}>{message}</div>
+      <table>
+        <tbody className={styles.tableContainer}>
+          {board.map((row, i) => (
+            <Row key={i} row={row} move={move} />
+          ))}
+        </tbody>
+      </table>
+
+      <div className={styles.buttonContainer}>
+        <div className={styles.button}>
+          <Button variant="contained" color="primary" onClick={() => {}}>
+            Restart
+          </Button>
+        </div>
+        <div className={styles.button}>
+          <Link to={`/`} className={styles.buttonText}>
+            <Button variant="contained" color="secondary">
+              Leave
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
-
-// class Game extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       player1: 1,
-//       player2: 2,
-//       currentPlayer: null,
-//       board: [],
-//       gameOver: false,
-//       message: "",
-//     };
-
-//     // Bind play function to App component
-//     this.play = this.play.bind(this);
-//   }
-
-//   // Starts new game
-//   initBoard() {
-//     // Create a blank 6x7 matrix
-
-//     this.setState({
-//       board,
-//       currentPlayer: this.state.player1,
-//       gameOver: false,
-//       message: "",
-//     });
-//   }
-
-//   play(c) {
-//     if (!this.state.gameOver) {
-//       // Place piece on board
-//       let board = this.state.board;
-//       for (let r = 5; r >= 0; r--) {
-//         if (!board[r][c]) {
-//           board[r][c] = this.state.currentPlayer;
-//           break;
-//         }
-//       }
-
-//       // Check status of board
-//       let result = this.checkAll(board);
-//       if (result === this.state.player1) {
-//         this.setState({
-//           board,
-//           gameOver: true,
-//           message: "Player 1 (red) wins!",
-//         });
-//       } else if (result === this.state.player2) {
-//         this.setState({
-//           board,
-//           gameOver: true,
-//           message: "Player 2 (yellow) wins!",
-//         });
-//       } else if (result === "draw") {
-//         this.setState({ board, gameOver: true, message: "Draw game." });
-//       } else {
-//         this.setState({ board, currentPlayer: this.togglePlayer() });
-//       }
-//     } else {
-//       this.setState({ message: "Game over. Please start a new game." });
-//     }
-//   }
-
-//   checkVertical(board) {
-//     // Check only if row is 3 or greater
-//     for (let r = 3; r < 6; r++) {
-//       for (let c = 0; c < 7; c++) {
-//         if (board[r][c]) {
-//           if (
-//             board[r][c] === board[r - 1][c] &&
-//             board[r][c] === board[r - 2][c] &&
-//             board[r][c] === board[r - 3][c]
-//           ) {
-//             return board[r][c];
-//           }
-//         }
-//       }
-//     }
-//   }
-
-//   checkHorizontal(board) {
-//     // Check only if column is 3 or less
-//     for (let r = 0; r < 6; r++) {
-//       for (let c = 0; c < 4; c++) {
-//         if (board[r][c]) {
-//           if (
-//             board[r][c] === board[r][c + 1] &&
-//             board[r][c] === board[r][c + 2] &&
-//             board[r][c] === board[r][c + 3]
-//           ) {
-//             return board[r][c];
-//           }
-//         }
-//       }
-//     }
-//   }
-
-//   checkDiagonalRight(board) {
-//     // Check only if row is 3 or greater AND column is 3 or less
-//     for (let r = 3; r < 6; r++) {
-//       for (let c = 0; c < 4; c++) {
-//         if (board[r][c]) {
-//           if (
-//             board[r][c] === board[r - 1][c + 1] &&
-//             board[r][c] === board[r - 2][c + 2] &&
-//             board[r][c] === board[r - 3][c + 3]
-//           ) {
-//             return board[r][c];
-//           }
-//         }
-//       }
-//     }
-//   }
-
-//   checkDiagonalLeft(board) {
-//     // Check only if row is 3 or greater AND column is 3 or greater
-//     for (let r = 3; r < 6; r++) {
-//       for (let c = 3; c < 7; c++) {
-//         if (board[r][c]) {
-//           if (
-//             board[r][c] === board[r - 1][c - 1] &&
-//             board[r][c] === board[r - 2][c - 2] &&
-//             board[r][c] === board[r - 3][c - 3]
-//           ) {
-//             return board[r][c];
-//           }
-//         }
-//       }
-//     }
-//   }
-
-//   checkDraw(board) {
-//     for (let r = 0; r < 6; r++) {
-//       for (let c = 0; c < 7; c++) {
-//         if (board[r][c] === null) {
-//           return null;
-//         }
-//       }
-//     }
-//     return "draw";
-//   }
-
-//   checkAll(board) {
-//     return (
-//       this.checkVertical(board) ||
-//       this.checkDiagonalRight(board) ||
-//       this.checkDiagonalLeft(board) ||
-//       this.checkHorizontal(board) ||
-//       this.checkDraw(board)
-//     );
-//   }
-
-//   componentWillMount() {
-//     this.initBoard();
-//   }
-
-//   render() {
-//     return (
-//       <div className={styles.container}>
-//         <table>
-//           <tbody className={styles.tableContainer}>
-//             {this.state.board.map((row, i) => (
-//               <Row key={i} row={row} play={this.play} />
-//             ))}
-//           </tbody>
-//         </table>
-
-//         <div className={styles.buttonContainer}>
-//           <div className={styles.button}>
-//             <Button
-//               variant="contained"
-//               color="primary"
-//               onClick={() => {
-//                 this.initBoard();
-//               }}
-//             >
-//               Restart
-//             </Button>
-//           </div>
-//           <div className={styles.button}>
-//             <Link to={`/`} className={styles.buttonText}>
-//               <Button variant="contained" color="secondary">
-//                 Leave
-//               </Button>
-//             </Link>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
 
 export default Game;
